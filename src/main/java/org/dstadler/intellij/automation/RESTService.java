1cb04f0b95e0bd54194722625c0f4bf40d639dad
Dominik Stadler <dominik.stadler@gmx.at>
2016-09-23 13:26:21 +0200

    Use logging and simplify creation of REST to IntelliJ action listeners

--- src/main/java/org/dstadler/intellij/automation/RESTService.java
+++ src/main/java/org/dstadler/intellij/automation/RESTService.java
@@ -1,6 +1,5 @@
 package org.dstadler.intellij.automation;
 
-import com.intellij.compiler.actions.CompileProjectAction;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -9,38 +8,41 @@ import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.containers.HashMap;
 import org.dstadler.commons.http.NanoHTTPD;
+import org.dstadler.commons.logging.jdk.LoggerFactory;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RESTService implements ProjectComponent {
+    private final static Logger logger = LoggerFactory.make();
+    
     private final Project project;
     private NanoHTTPD nanoHTTPD;
 
     public RESTService(Project project) {
-        System.out.println("Started with project " + project + ": " + project.getName());
+        logger.info("Started with project " + project + ": " + project.getName());
         this.project = project;
     }
 
     @Override
     public void initComponent() {
-        //e.getActionManager().tryToExecute(e.getActionManager().getAction("CompileProject"), new MenuKeyEvent(), null, null, true);
-        // TODO: insert component initialization logic here
-
-        System.out.println("Had action: " + ApplicationManager.getApplication().getComponent(CompileProjectAction.class));
-        System.out.println("Had 2action: " + ActionManager.getInstance().getAction("CompileProject"));
+        logger.info("Starting REST server to serve actions via REST");
 
         Map<String,ActionListener> actionMap = new HashMap<>();
-        actionMap.put("Recompile", new RecompileActionListener());
-        actionMap.put("Compile", new CompileActionListener());
+
+        // map actionIds to REST paths
+        actionMap.put("Recompile", new AnActionActionListener("CompileProject"));
+        actionMap.put("Compile", new AnActionActionListener("CompileDirty"));
 
         try {
             nanoHTTPD = RESTServer.create(actionMap);
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "Could not start the REST server", e);
         }
     }
 
@@ -48,6 +50,7 @@ public class RESTService implements ProjectComponent {
     public void disposeComponent() {
         // shut down the REST interface
         if(nanoHTTPD != null) {
+            logger.info("Shutting down REST Server");
             nanoHTTPD.stop();
             nanoHTTPD = null;
         }
@@ -69,31 +72,22 @@ public class RESTService implements ProjectComponent {
         // called when project is being closed
     }
 
-    private class RecompileActionListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            AnActionEvent event = AnActionEvent.createFromDataContext("RecompileAction", null, dataId -> {
-                if (dataId.equals(CommonDataKeys.PROJECT.getName())) {
-                    return project;
-                }
-                return null;
-            });
+    private class AnActionActionListener implements ActionListener {
+        private final String actionId;
 
-            ApplicationManager.getApplication().invokeLater(() -> ActionManager.getInstance().getAction("CompileProject").actionPerformed(event));
+        public AnActionActionListener(String actionId) {
+            this.actionId = actionId;
         }
-    }
-
-    private class CompileActionListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            AnActionEvent event = AnActionEvent.createFromDataContext("CompileAction", null, dataId -> {
+            AnActionEvent event = AnActionEvent.createFromDataContext(actionId, null, dataId -> {
                 if (dataId.equals(CommonDataKeys.PROJECT.getName())) {
                     return project;
                 }
                 return null;
             });
 
-            ApplicationManager.getApplication().invokeLater(() -> ActionManager.getInstance().getAction("CompileDirty").actionPerformed(event));
+            ApplicationManager.getApplication().invokeLater(() -> ActionManager.getInstance().getAction(actionId).actionPerformed(event));
         }
     }
 }
